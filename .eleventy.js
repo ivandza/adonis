62963c85303918004db6c26119f4dd746a17ec6d
Ivan <ivandzal7@gmail.com>
2025-09-06 04:29:05 +0200

    first version

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+// .eleventy.js
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("images"); // if you have this
+  return {
+    dir: { input: ".", includes: "_includes", output: "_site" },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+};
